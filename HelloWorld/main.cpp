56f2b72a01d2e8678c169afcb26b860160fad5fe
Vikram Nagarkar <vikramnagarkar@vikrams-mbp.home>
2019-11-08 15:32:31 +0000

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Vikram Nagarkar on 08/11/2019.
+//  Copyright © 2019 Vikram Nagarkar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
